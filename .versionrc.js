a042f687c8d5a29ba3990bb333ca2edbdc23f987
kasty <mariegosse@gmail.com>
2023-05-23 15:28:45 +0200

    VAST-43 add package-lock to bumpFiles

--- .versionrc.js
+++ .versionrc.js
@@ -43,6 +43,10 @@ const config = () => ({
   bumpFiles: [{
     filename: 'package.json',
     type: 'json'
+  },
+  {
+    filename: 'package-lock.json',
+    type: 'json'
   }],
   scripts: {
     prerelease: oneLiner(' && ')([
